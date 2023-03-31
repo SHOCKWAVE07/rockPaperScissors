772788ecccff2adc33fbff97b0ae6a7ad352a01c
SHOCKWAVE07 <varunraskar22@gmail.com>
2023-03-31 15:03:43 +0530

    Programming is over

--- index.js
+++ index.js
@@ -1,59 +1,85 @@
-
-function getComputerChoice(){
-    const a = ["rock","paper","scissors"]
-    const choice = Math.floor(Math.random()*a.length)
-    return a[choice]
-}
-
-function playerSelection(){
-    const selection = prompt("Enter your choice").toLowerCase();
-    return selection
-}
-
-function playRound(player,pc){
-    if(player===pc){
-        return -1
+function getComputerChoice() {
+    const a = ["rock", "paper", "scissors"];
+    const choice = Math.floor(Math.random() * a.length);
+    return a[choice];
+  }
+  
+  function playerSelection(selection) {
+    
+    selection = selection.toLowerCase();
+    
+  
+    return selection;
+  }
+  
+  function playRound(player, pc) {
+    if (player === pc) {
+      return -1;
+    } else if (
+      (player === "rock" && pc === "scissor") ||
+      (player === "paper" && pc === "rock") ||
+      (player === "scissor" && pc === "paper")
+    ) {
+      return 1;
+    } else {
+      return 0;
     }
-    else if (
-        (player === "rock" && pc === "scissors") ||
-        (player === "paper" && pc === "rock") ||
-        (player === "scissors" && pc === "paper")
-      ) {
-        return 1;
-      } else {
-        return 0;
-      }
-}
-
-function game(){
-    let rounds=5
-    let player = 0
-    let pc = 0
-    while(rounds>0){
-        const playerChoice = playerSelection()
-        const pcChoice = getComputerChoice()
-        const play=playRound(playerChoice,pcChoice)
-        if(play===1){
-            player++
+  }
+  
+  function game() {
+    let rounds = 5;
+    let playerScore = 0;
+    let pcScore = 0;
+    let gameover=false
+    const bans = document.querySelectorAll(".container button");
+    bans.forEach((btn) =>
+      btn.addEventListener("click", function () {
+        const playerChoice = playerSelection(btn.textContent);
+        const pcChoice = getComputerChoice();
+  
+        const play = playRound(playerChoice, pcChoice);
+        if(gameover==true){
+            return;
         }
-        else if(play===-1){
-            console.log("It's a tie")
+        if (play === 1) {
+          playerScore++;
+        } else if (play === -1) {
+            const h3 = document.createElement("h3")
+            const container = document.querySelector(".container");
+            h3.textContent=`It's Tie`;
+            container.appendChild(h3);
+        } else {
+          pcScore++;
         }
-        else{
-            pc++
+        rounds--;
+
+        const h2 = document.createElement("h2")
+        const container = document.querySelector(".container");
+        h2.textContent=`Player: ${playerScore}  PC: ${pcScore}`;
+        container.appendChild(h2);
+
+        if (rounds === 0) {
+          let winner = null;
+          if (playerScore > pcScore) {
+            winner = "Player";
+          } else {
+            winner = "PC";
+          }
+        const h1 = document.createElement("h1")
+        const container = document.querySelector(".container");  
+        h1.textContent=`Final Score:-  Player: ${playerScore}  PC: ${pcScore}  The ${winner} wins`;
+        container.appendChild(h1);
+        
+        gameover=true
+
+        setTimeout(function() {
+        location.reload();
+        }, 3000);
+      
+       
+
         }
-        rounds--
-        console.log(`Player: ${player}  PC: ${pc}`)
-    }
-    let winner = null
-    if(player>pc){
-        winner = "Player"
-    }
-    else{
-        winner = "PC"
-    }
-    console.log(`Final Score:-  Player: ${player}  PC: ${pc}  The ${winner} wins`)
-}
-
-game()
-
+      })
+    );
+  }
+  game();
